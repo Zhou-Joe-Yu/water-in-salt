c562c6a8e57b8504a1a1ea00a99f2b9db95f2a8f
Zhou (Joe) Yu <80474660+Zhou-Joe-Yu@users.noreply.github.com>
2021-03-11 10:48:17 -0600

    Update get_Li_msd.m

--- get_Li_msd.m
+++ get_Li_msd.m
@@ -1,3 +1,4 @@
+%%%This code is used to calculate the diffusivity of ion through the mean square displacement calculated from the Gromacs
 clear all;
 close all;
 
@@ -5,7 +6,6 @@ n = 5;
 
 for i = 1 : n
     filename = ['msd_o' num2str(i) '.xvg'];
-    %filename = ['msd_lat_C1' num2str(i) '.xvg'];
     EL{i} = readfile(filename,19);
 end
 
@@ -25,16 +25,12 @@ end
 
 EL_yfirst=(EL{1}(:,2)+EL{2}(:,2)+EL{3}(:,2))/3;
 EL_xfirst=EL{1}(:,1);
-save poly_na_71_x.dat EL_xfirst -ascii
-save poly_na_71_y.dat EL_yfirst -ascii
+save EL_x.dat EL_xfirst -ascii
+save EL_y.dat EL_yfirst -ascii
 
-%xlabel('Time');
-%ylabel('polymer com MSD');
-%legend('polymer com');
-%axis([1 3*10^5 10^-4 10]);
 set(gca,'fontsize',15,'fontweight','bold');
 set(gca,'linewidth',1);
 set(get(gca,'xlabel'),'FontSize', 15, 'FontWeight', 'Bold','Fontname','Times New Roman');
 set(get(gca,'ylabel'),'FontSize', 15, 'FontWeight', 'Bold','Fontname','Times New Roman');
-%print('-dpng','-r300','msd_poly_com_71.png');
+print('-dpng','-r300','msd_EL.png');
 
