71ba5b9eb3bfdbee765bba4cb0718b9f589d053a
Zhou (Joe) Yu <80474660+Zhou-Joe-Yu@users.noreply.github.com>
2021-03-11 10:56:24 -0600

    Update build_index.m

--- build_index.m
+++ build_index.m
@@ -1,14 +1,19 @@
+%%build index 
 clear all
 close all
 
+initial_index=2000;
+ion_number=2000;
+atom_in_ion=15;
+
 fid=fopen('index_tfsi.ndx','w');
 
-for i=1:2000
+for i=1:ion_number
     
     fprintf(fid,'%s %d %s\n','[',i,']');
     
-    for j=1:15
-        fprintf(fid,'%d ',2000+15*(i-1)+j);
+    for j=1:atom_in_ion
+        fprintf(fid,'%d ',initial_index+atom_in_ion*(i-1)+j);
     end    
     
     fprintf(fid,'\n');
